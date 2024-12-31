5a1f917ff39c3a354b6ea5328b362d0bcb403aab
Alex Javier <alexavier1989@gmail.com>
2024-12-30 23:13:42 -0500

    feat: Tasks Part 1 done. Js experiments added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
-// Prevent us from attempting to use variables
-// that are not declared
 "use strict"
+/*
+
+// Create an array that holds different data types and log it to the console
+const mixedArray = [42, "Hello", true, null, { name: "John" }, [1, 2, 3]];
+console.log("Mixed Array: ", mixedArray); // The array accepts any data type (number, string, boolean, object, array, etc.).
+
+// Log the typeof an array to the console
+console.log("Type of mixedArray: ", typeof mixedArray);  // Output: "object" because arrays in JavaScript are a special type of object.
+
+// Remove an item from the array
+mixedArray.pop(); // Removes the last item
+console.log("Array after removing an item: ", mixedArray); // Output: [42, "Hello", true, null, { name: "John" }]
+
+// Add an item to the array
+mixedArray.push("New item"); 
+console.log("Array after adding an item: ", mixedArray); // Output: [42, "Hello", true, null, { name: "John" }, "New item"]
+
+// Change the value of an element in the array
+mixedArray[1] = "New value";
+console.log("Array after changing an item: ", mixedArray); // Output: [42, "New value", true, null, { name: "John" }, "New item"]
+
+
+*/
